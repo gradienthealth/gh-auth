34be443a7b2c823c262064832de9a6909910860c
millerj1994 <jmm146@duke.edu>
2021-04-14 16:01:12 -0500

    Fixed url.

--- ghauth/__init__.py
+++ ghauth/__init__.py
@@ -1,7 +1,7 @@
 import requests
 
 def credentials():
-	BASE_URL = 'https://uax8u4j8a0.execute-api.us-east-1.amazonaws.com/dev'
+	BASE_URL = 'https://pse8jrgreg.execute-api.us-east-1.amazonaws.com/dev'
 	LOGIN_URL = '{}/login'.format(BASE_URL)
 	SESSION_URL = '{}/me'.format(BASE_URL)
 	print('Visit {} to get a session token'.format(LOGIN_URL))
